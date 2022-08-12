ea7fc4a352cfb126951153facdb53887632d07e6
drumnistnakano <syxqh349ysykn@gmail.com>
2022-08-12 12:29:56 +0900

    Fix: 未完了リストの上限値の機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p>登録できる TODO の上限は 5 個です。</p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
